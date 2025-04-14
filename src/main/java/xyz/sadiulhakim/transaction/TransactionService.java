eb792c0322223768535c3a588e78cb032d8f6293
Sadiul Hakim <sadiulhakim@gmail.com>
2025-04-14 14:10:19 +0600

    Transaction

--- src/main/java/xyz/sadiulhakim/transaction/TransactionService.java
+++ src/main/java/xyz/sadiulhakim/transaction/TransactionService.java
@@ -1,6 +1,7 @@
 package xyz.sadiulhakim.transaction;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
@@ -22,5 +23,39 @@ public class TransactionService {
 		var currency = Currency.get(dto.currency());
 		var transaction = new Transaction(user, dto.amount(), type, currency, LocalDateTime.now());
 		repository.save(transaction);
+	}
+
+	public List<Transaction> findAllTransactionsOfUser(long userId) {
+		var user = userService.findById(userId);
+		return repository.findAllByUser(user);
+	}
+
+	public List<Transaction> findAllTransactionsOfUserByType(long userId, String type) {
+		var user = userService.findById(userId);
+		var typeEnum = TransactionType.get(type);
+		return repository.findAllByUserAndType(user, typeEnum);
+	}
+
+	public List<Transaction> findAllTransactionsOfUserByTypeAndCurrency(long userId, String type, String currency) {
+		var user = userService.findById(userId);
+		var typeEnum = TransactionType.get(type);
+		var currencyEnum = Currency.get(currency);
+		return repository.findAllByCurrencyAndUserAndType(currencyEnum, user, typeEnum);
+	}
+
+	public List<Transaction> findAllTransactionsOfUserByTypeAndCurrencyAtATime(long userId, String type,
+			String currency, LocalDateTime time) {
+		var user = userService.findById(userId);
+		var typeEnum = TransactionType.get(type);
+		var currencyEnum = Currency.get(currency);
+		return repository.findByTimeAndCurrencyAndUserAndType(time, currencyEnum, user, typeEnum);
+	}
+
+	public List<Transaction> findAllTransactionsOfUserByTypeAndCurrencyBetweenTimes(long userId, String type,
+			String currency, LocalDateTime time1, LocalDateTime time2) {
+		var user = userService.findById(userId);
+		var typeEnum = TransactionType.get(type);
+		var currencyEnum = Currency.get(currency);
+		return repository.findByTimeBetweenAndCurrencyAndUserAndType(time1, time2, currencyEnum, user, typeEnum);
 	}
 }
